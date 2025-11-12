a8387c5279e0700cf95c7d29e7cb8fcaac5446ed
Paul Leroy <paul.leroy@univ-rennes1.fr>
2025-11-12 12:24:15 +0100

    Update qM3C2Process.cpp

--- plugins/core/Standard/qM3C2/src/qM3C2Process.cpp
+++ plugins/core/Standard/qM3C2/src/qM3C2Process.cpp
@@ -290,7 +290,8 @@ CCVector3 computeNormal(CCCoreLib::DgmOctree::NeighboursSet neighbours, CCCoreLi
 		//we determine the plane normal by computing the smallest eigen value of M = 1/n * S[(p-µ)*(p-µ)']
 		CCCoreLib::SquareMatrixd eigVectors;
 		std::vector<double> eigValues;
-		if (CCCoreLib::Jacobi<double>::ComputeEigenValuesAndVectors(Z.computeCovarianceMatrix(), eigVectors, eigValues, true))
+        CCCoreLib::SquareMatrixd covarianceMatrix = Z.computeCovarianceMatrix();
+        if (CCCoreLib::Jacobi<double>::ComputeEigenValuesAndVectors(covarianceMatrix, eigVectors, eigValues, true))
 		{
 			/*** code and comments below are from the original 'm3c2' code (N. Brodu) ***/
 
